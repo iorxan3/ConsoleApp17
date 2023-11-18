6ff8dae2749a90472b209ef957296ecc5d492dff
Orxan <ibrahimzada003@gmail.com>
2023-11-18 13:45:57 +0400

    Add project files.

---
+++ ConsoleApp17/Models/M416.cs
@@ -0,0 +1,68 @@
+﻿namespace M416.Models
+{
+    public class Atis
+    {
+        public int GunCount = 60;
+        private object consoleKey;
+
+        public void AtisTekTek()
+        {
+            int Sarsor = 60;
+                GunCount--;
+            Console.WriteLine("Atis zamani gulle azalir 1 eded {GunCount} /{Sarsor}");
+
+
+            if (GunCount == 0)
+            {
+                Console.WriteLine("Gulle sayi azalir..");
+                YenidenDoldurma();
+                return;
+            }
+        }
+        public void YenidenDoldurma()
+        {
+            if (GunCount == 32)
+            {
+                Console.WriteLine("Doludur..");
+                return;
+            }
+            GunCount = 32;
+        }
+        public void Atis33()
+        {
+            int Sarsor = 60;
+            GunCount = GunCount - 3;
+            Console.WriteLine("Uc-uc azalir {GunCount} /{Sarsor}");
+
+            if (GunCount == 0)
+            {
+                Console.WriteLine("Daraqdaki gulle sayi azalir..");
+                YenidenDoldurma();
+                return;
+            }
+        }
+        public void Metod()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo Metodinfo = Console.ReadKey();
+                switch (Metodinfo.Key)
+                {
+                    case ConsoleKey.A:
+                        AtisTekTek();
+                        break;
+                    case ConsoleKey.B:
+                        Atis33();
+                        break;
+                    case ConsoleKey.C:
+                        YenidenDoldurma();
+                        break;
+                }
+            }
+        }
+        public void SilahiCagirmaq()
+        {
+            Metod();
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,6 @@
+﻿#region task2
+using M416.Models;
+Atis silahAtisi = new();
+silahAtisi.SilahiCagirmaq();
+
+#endregion
